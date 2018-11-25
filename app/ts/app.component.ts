340a61981889fb4d40c09d5fd1544060c4ed698d
Duke Ovie <dukeovie09@gmail.com>
2018-11-25 12:05:50 +0100

    app newly setup

---
+++ app/ts/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+import {Config} from './config.service';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>{{ mainHeading }}</h1>'
+})
+
+export class AppComponent {
+    mainHeading = Config.MAIN_HEADING;
+}
